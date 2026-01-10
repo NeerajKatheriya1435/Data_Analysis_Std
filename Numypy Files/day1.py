b6ac37e65f397e47cc2638cff86af43ba13d7aea
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-01-10 18:45:30 +0530

    Filtered files

---
+++ Numypy Files/day1.py
@@ -0,0 +1,53 @@
+# inp1=6
+# inp2=7
+# print(inp1+inp2)
+
+# inp3=6
+# inp4=8
+# print(inp3+inp4)
+
+
+
+# def func():
+#     fact =int(input("Enter ypou number: "))
+#     ans=1
+#     for i in range(1,fact+1):
+#         ans=ans*i
+#     print(ans)
+
+# func()
+# func()
+
+# def addTwonum(num1,num2): # Formal Parameter
+#     print(num1+num2)
+
+# addTwonum(3,5) # actual prameter
+# addTwonum(3,6)
+
+
+def func1(num1,num2):
+    # print("Hello The sum is: ")
+    # print(num1+num2)
+    return (num1+num2)
+
+# func1(3,7)
+# print(func1(13,7))
+
+# def greet():
+#     print("hello")
+#     return "hii"
+
+# print(greet())
+
+import numpy as np
+# var1=np.array([4,5,6,7])
+# print(var1)
+
+# list1=[4,5,7,3]
+# list1=[[]]
+# print(list1)
+var1=[i*2 for i in range(1,21)]
+print(var1)
+
+var2=np.arange(1,21)*2
+print(var2)
