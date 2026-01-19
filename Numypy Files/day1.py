900b1116edfd6d7d09f852159e47101a9e380e96
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-01-19 15:29:06 +0530

    numpy started

--- Numypy Files/day1.py
+++ Numypy Files/day1.py
@@ -25,29 +25,45 @@
 # addTwonum(3,6)
 
 
-def func1(num1,num2):
-    # print("Hello The sum is: ")
-    # print(num1+num2)
-    return (num1+num2)
+# def func1(num1,num2):
+#     # print("Hello The sum is: ")
+#     # print(num1+num2)
+#     return (num1+num2)
 
-# func1(3,7)
-# print(func1(13,7))
+# # func1(3,7)
+# # print(func1(13,7))
 
-# def greet():
-#     print("hello")
-#     return "hii"
+# # def greet():
+# #     print("hello")
+# #     return "hii"
 
-# print(greet())
+# # print(greet())
 
-import numpy as np
-# var1=np.array([4,5,6,7])
+# import numpy as np
+# # var1=np.array([4,5,6,7])
+# # print(var1)
+
+# # list1=[4,5,7,3]
+# # list1=[[]]
+# # print(list1)
+# var1=[i*2 for i in range(1,21)]
 # print(var1)
 
-# list1=[4,5,7,3]
-# list1=[[]]
-# print(list1)
-var1=[i*2 for i in range(1,21)]
-print(var1)
+# var2=np.arange(1,21)*2
+# print(var2)
 
-var2=np.arange(1,21)*2
-print(var2)
+import numpy as np
+
+# num1=np.array([4,5,6,7,87])
+# print(num1)
+# print(type(num1))
+
+# l1=[5,6,7,87]
+# print(l1)
+
+# for i in range(1,10):
+
+# l1=[i*2 for i in range(1,10)]
+# print(l1)
+
+print(np.arange(1,21)*2)
